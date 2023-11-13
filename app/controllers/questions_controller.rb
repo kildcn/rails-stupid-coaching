4c5536918363508115126a728d0b978a012d87eb
Killian Le Doucen <killian.ledoucen@yahoo.fr>
2023-11-13 16:26:32 +0100

    Style added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,9 +5,9 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     if params[:question] == 'I am going to work'
-      @anwer = "Great!"
+      @anwer = 'Great!'
     elsif params[:question].end_with?('?')
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
